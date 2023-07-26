640f3c0110e7cf2b279e4c0ad8fd7cdd490b0982
lsw071394 <54699628+lsw071394@users.noreply.github.com>
2023-07-26 16:10:52 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,216 +1,183 @@
-console.log("Script Load");
-(function ($) {
-    $(document).ready(function () {
-        console.log('jQuery Ready');
-        var _scrollTop = null;
-        var _isAni = void 0; // undefined 강제 지정
-        var transitionEnd = true;
-        function init() {
-            layout();
-            setting();
-            addEvent();
-        }
-        function layout() {
-            // 브라우저 창 셀렉터.
-            $win = $(window);
+var bodyEl = document.querySelector('body');
+var gearEl = document.querySelector('.e');
 
-            // 스크롤 애니메이션을 위한 셀렉터.
-            $scroll = $('html, body');
+var rotateEl = document.querySelectorAll('#rotate');
+var reverseRotateEl = document.querySelectorAll('#reverse-rotate');
+var scrolliconEl = document.querySelector('.scrollicon');
 
-            // 메뉴 셀렉터.
-            $gnEl = $('.Menu a');
-            // console.log($gnEl);
-            $section = $('section');
-            // 섹션 셀렉터.
-            $article = $('article');
-            // console.log($section);
-        }
+// 햄버거 메뉴
+var MenuEl = document.querySelector('.hamburger');
+var spanEL = document.querySelectorAll('span');
+var navEl = document.querySelector('nav');
+var navMenuEl = document.querySelector('div.Menu');
+var hamburger = document.querySelector('.hamburger a');
+var toggle = true;
 
-        function setting() {
-            _isAni = false;
-        }
+// 풀페이지
+var articleEl = document.querySelectorAll("article"); // article 변수지정
+articleEl = Array.prototype.slice.call(articleEl);
+var current = 0;
+var transitionEnd = true;
 
-        function addEvent() {
-            $win.on('resize', onResize);
-            $win.on('scroll', onScrollWin).trigger('scroll');
-            $('nav a').on('click', onClick);
-        }
-        function onResize(e) {
-            $('article').css({ 'height': 100 + 'vh' })
-        };
+// skill
+var imgEls = document.querySelectorAll('.photo-box>img')
+Array.prototype.slice.call('imgEl');
+var progerssEls = document.querySelectorAll('.progress-bar-fill');
+Array.prototype.slice.call('progressEls');
+var section = document.querySelector('section');
 
+// Accordion
+var htmlEl = document.documentElement,
+    accordianEl = document.querySelector("#accordion"),
+    viewItemEls = accordianEl.querySelectorAll(".view-item"),
+    btnCloseEls = accordianEl.querySelectorAll(".view-item > .btn-close"),
+    _cuId = null,
+    _exId = null,
+    _timer;
+
+var contactEl = document.querySelector('.contact');
+var contactbodyEl = document.querySelector('.contactbody');
+var gear = document.querySelectorAll('.gear');
+// reset();
+// function reset() {
+//     navEl.style.transform = 'translateX(100%)';
+//     contactbodyEl.style.transform = 'translate(-50%,-50%) scale(0)';
+// }
+window.onload = function () {
+    setTimeout(function () {
+        scrollTo(0, 0);
+        articleEl[0].style.visibility = 'visible';
+    }, 50);
+}
+// 메인 톱니바퀴 모션 함수
+setTimeout(function () {
+    gearEl.style.animation = 'rotateGear 2s linear infinite forwards';
+    for (var i = 0; i < rotateEl.length; i++) {
+        rotateEl[i].style.animation = 'rotation 2s linear infinite forwards';
+    }
+    for (var i = 0; i < reverseRotateEl.length; i++) {
+        reverseRotateEl[i].style.animation = 'reverse-rotation 2s linear infinite forwards';
+    }
+}, 2100);
+
+setTimeout(function () {
+    scrolliconEl.style.opacity = '1';
+}, 2500);
+
+// 햄버거 클릭시
+function MenuChange(e) {
+    e.preventDefault();
+    openmenu();
+    for (i = 0; i < spanEL.length; i++) {
+        spanEL[i].style.transition = 'all 0.5s ease-in-out';
+    }
+    if (toggle) {
+        spanEL[0].style.transform = 'translateY(9px) rotateZ(45deg)';
+        spanEL[1].style.opacity = 0;
+        spanEL[2].style.transform = 'translateY(-9px) rotateZ(-45deg)';
+        hamburger.style.background = '#444444';
+        hamburger.style.boxShadow = '5px 5px 9px #2b2b2b inset, -5px -5px 9px #5d5d5d inset ';
+    } else if (!toggle) {
+        spanEL[0].style.transform = '';
+        spanEL[1].style.opacity = 1;
+        spanEL[2].style.transform = '';
+        hamburger.style.background = 'linear-gradient(145deg, #ffffff, #e6e6e6)';
+        hamburger.style.boxShadow = '5px 5px 9px #a1a1a1';
+    }
+    toggle = !toggle;
+}
+MenuEl.addEventListener('click', MenuChange);
+
+fatimesEl = document.querySelector('.fa-times');
+// fatimesEl.addEventListener('click', closecontact);
+// contactEl.addEventListener('click', opencontact);
+function closecontact() {
+    for (var i = 0; i < gear.length; i++) {
+        gear[i].style.opacity = '0.3';
+        gear[i].style.transition = 'all .5s ease-in'
+    }
+    contactbodyEl.style.transform = 'translate(-50%,-50%) scale(0)';
+    contactbodyEl.style.transition = 'all 0.3s ease-in-out';
+}
+
+
+function opencontact(e) {
+    for (var i = 0; i < gear.length; i++) {
+        gear[i].style.opacity = '1';
+        gear[i].style.transition = 'all 1s ease-in'
+    }
+    contactbodyEl.style.transform = 'translate(-50%,-50%) scale(1)';
+    contactbodyEl.style.opacity = '1';
+    // contactbodyEl.style.transform = 'translateY(0)';
+    contactbodyEl.style.transition = 'all 0.3s ease-in-out';
+}
+
+
+// 메뉴 오픈시
+function openmenu() {
+    if (toggle) {
+        navEl.style.transform = 'scale(1)';
+        navEl.style.opacity = '1';
+        navEl.style.transform = 'translateX(0)';
+        navMenuEl.style.display = 'block';
+        navMenuEl.style.opacity = '0';
+        navMenuEl.style.transition = 'opacity 0.3s ease-in-out';
         setTimeout(function () {
-            $('article').on('wheel', fullpage), { passive: false };
-        }, 2700);
-        function onScrollWin(e) {
-            // console.log('window scroll');
-            _scrollTop = $win.scrollTop(); // 스크롤 Y 좌표.
-            // console.log(_scrollTop);
+            navMenuEl.style.opacity = '1';
+        }, 500);
+    }
+    else if (!toggle) {
+        navEl.style.transform = 'scale(0)';
+        navEl.style.opacity = '0';
+        navMenuEl.style.opacity = '0';
+        navMenuEl.style.visibilty = 'hidden';
+        navMenuEl.style.transition = 'opacity 0.3s ease-in-out';
+        navEl.style.transform = 'translateX(100%)';
+    }
+    navEl.style.transition = 'all 0.3s ease-in-out';
+}
 
-            if (!_isAni) {
-                scrollMenuActive();
-            }
-            // 스크롤 이벤트 핸들러의 가장 마지막 부분에 저장.
-            // 이전 스크롤 Y 좌표.
-
-        }
-
-        // 스크롤 시 메뉴 활성화 변경 기능.
-        function scrollMenuActive() {
-            // $.each();
-            // $section.each();
-            $.each($article, function (index, el) {
-                // console.log(index, el);
-                var $el = $(el), // 각 요소 셀렉터.
-                    start, // 요소가 시작하는 스크롤 시점.
-                    end; // 요소가 끝나는 스크롤 시점.
-
-
-                start = $el.offset().top;
-
-                end = start + $el.innerHeight();
-                if (index === 0) {
-                    start = 0;
-                }
-                // console.log(index, start, end); 
-                // 스크롤 값과 해당 섹션의 위치를 비교해서 활성화 여부를 판단(조건문).
-                if (_scrollTop >= start && _scrollTop < end) {
-                    if (!$gnEl.eq(index).hasClass('active')) {
-                        $gnEl.removeClass('active');
-                        $gnEl.eq(index).addClass('active');
-                    }
-                }
-            });
-        }
-        var $imgels = $('#skill .photo-box img');
-        var $progressEl = $('.progress-bar-fill')
-        function progress() {
-            if (count == 3) {
-                setTimeout(function () {
-                    $imgels.eq(0).css({ 'opacity': 1, 'transition': 'all 1s ease-in-out', 'animation': 'hovering 1s infinite ease-in-out' });
-                    $imgels.eq(1).css({ 'opacity': 1, 'transition': 'all 1.5s ease-in-out', 'animation': 'hovering 1s infinite ease-in-out' });
-                    $imgels.eq(2).css({ 'opacity': 1, 'transition': 'all 2s ease-in-out', 'animation': 'hovering 1s infinite ease-in-out' });
-                    $imgels.eq(3).css({ 'opacity': 1, 'transition': 'all 2.5s ease-in-out', 'animation': 'hovering 1s infinite ease-in-out' });
-                    $imgels.eq(4).css({ 'opacity': 1, 'transition': 'all 3s ease-in-out', 'animation': 'hovering 1s infinite ease-in-out' });
-                    $imgels.eq(5).css({ 'opacity': 1, 'transition': 'all 3.5s ease-in-out', 'animation': 'hovering 1s infinite ease-in-out' });
-                }, 1500);
-                setTimeout(function () {
-                    $progressEl.eq(0).animate({ width: '80%', backgroundColor: 'black' }, { duration: 1000 });
-                    $progressEl.eq(1).animate({ width: '60%', backgroundColor: 'black' }, { duration: 1000 });
-                    $progressEl.eq(2).animate({ width: '65%', backgroundColor: 'black' }, { duration: 1000 });
-                    $progressEl.eq(3).animate({ width: '50%', backgroundColor: 'black' }, { duration: 1000 });
-                    $progressEl.eq(4).animate({ width: '50%', backgroundColor: 'black' }, { duration: 1000 });
-                }, 1000);
-            }
-            if (count >= 1) {
-                $('.gear').animate({ opacity: 0.3 }, { duration: 500 })
-            } else if (count < 1) {
-                $('.gear').animate({ opacity: 1 }, { duration: 500 })
-            }
-        }
-        function onClick(e) {
-            e.preventDefault();
-            var index = $("nav a").index(this);
-            console.log(index);
-            var href = $(this).attr('href')
-            var top = $(href).offset().top
-            $('html,body').animate({
-                'scrollTop': top
-            }, { duration: 500 })
-            count = index;
-            progress();
-
-        }
-        var h = 0;
-        $(window).scroll(function () {
-            var scrT = parseInt($(this).scrollTop());
-            h = $(window).height();
-            $(window).resize(function () {
-                h = $(window).height()
-            });
-
-            $('article').each(function (i) {
-                var secTop = $(this).offset().top;
-
-                if (secTop <= scrT) {
-                    $('nav ul li a').removeClass('on');
-                    $('nav ul li').eq(i).find('a').addClass('on');
-                };
-            });
-        });
-
-        function tEnd() {
-            transitionEnd = false;
-            setTimeout(function () {
-                transitionEnd = true;
-            }, 1000);
-        }
-       
-        var count = 0;
-
-        if($(window).innerWidth() > 1100){
-            function fullpage(e) {
-                e.preventDefault();
-    
-                if (!transitionEnd) {
-                    return;
-                };
-                // var m = e.originalEvent.wheelDelta;
-                if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
-                    if (count > 0) {
-                        count--;
-                        console.log(count);
-                        var prev = $(this).prev().offset().top;
-                        $('html,body').stop().animate({
-                            'scrollTop': prev
-                        }, { duration: 500 });
-                        tEnd();
-                    }
-                }
-                else {
-                    if (count < $('article').length - 1) {
-                        count++;
-                        console.log(count);
-                        var next = $(this).next().offset().top;
-                        $('html,body').stop().animate({
-                            'scrollTop': next
-                        }, { duration: 500 });
-                        tEnd();
-                    }
-                }
-                progress();
-            };
-        }
-       
-        
-        if($(window).innerWidth() < 1100){
-            $('html').css({'overflow' : 'scroll'})
-            $('article').remove('wheel', fullpage);
-            setTimeout(function () {
-                $imgels.eq(0).css({ 'opacity': 1, 'transition': 'all 1s ease-in-out', 'animation': 'hovering 1s infinite ease-in-out' });
-                $imgels.eq(1).css({ 'opacity': 1, 'transition': 'all 1.5s ease-in-out', 'animation': 'hovering 1s infinite ease-in-out' });
-                $imgels.eq(2).css({ 'opacity': 1, 'transition': 'all 2s ease-in-out', 'animation': 'hovering 1s infinite ease-in-out' });
-                $imgels.eq(3).css({ 'opacity': 1, 'transition': 'all 2.5s ease-in-out', 'animation': 'hovering 1s infinite ease-in-out' });
-                $imgels.eq(4).css({ 'opacity': 1, 'transition': 'all 3s ease-in-out', 'animation': 'hovering 1s infinite ease-in-out' });
-                $imgels.eq(5).css({ 'opacity': 1, 'transition': 'all 3.5s ease-in-out', 'animation': 'hovering 1s infinite ease-in-out' });
-                $progressEl.eq(0).animate({ width: '80%', backgroundColor: 'black' }, { duration: 1000 });
-                $progressEl.eq(1).animate({ width: '60%', backgroundColor: 'black' }, { duration: 1000 });
-                $progressEl.eq(2).animate({ width: '65%', backgroundColor: 'black' }, { duration: 1000 });
-                $progressEl.eq(3).animate({ width: '50%', backgroundColor: 'black' }, { duration: 1000 });
-                $progressEl.eq(4).animate({ width: '50%', backgroundColor: 'black' }, { duration: 1000 });
-            }, 1000);
-            $(window).scroll(function () {
-                var height = $(document).scrollTop();
-                console.log(height);
-                if (height >= 200) {
-                    $('.gear').animate({ opacity: 0.3 }, { duration: 100 })
-                } else if (height < 200) {
-                    $('.gear').animate({ opacity: 1 }, { duration: 100 })
-                }
-            });
-        }
+viewItemEls = Array.prototype.slice.call(viewItemEls);
+function onResize() {
+    clearInterval(_timer);
+    htmlEl.classList.add("on-resize");
+    _timer = setTimeout(function () {
+        htmlEl.classList.remove("on-resize");
+    }, 1);
+}
+function onClickViewItem(e) {
+    e.preventDefault();
+    var el = e.currentTarget, index = viewItemEls.indexOf(el);
+    if (!el.classList.contains("selected")) {
+        _cuId = index;
+        if (_exId !== null) viewItemEls[_exId].classList.remove("selected");
+        el.classList.add("selected");
+        _exId = _cuId;
+    } else if (el.classList.contains("selected")) {
+        el.classList.remove('selected');
+    }
+}
+function onClickClose(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    var el = e.currentTarget, itemEl = el.parentElement;
+    if (itemEl.classList.contains("selected")) {
+        if (_exId !== null) viewItemEls[_exId].classList.remove("selected");
+        _cuId = null;
+        _exId = _cuId;
+    }
+}
+function addEvent() {
+    window.addEventListener("resize", onResize);
+    for (var i = 0; i < viewItemEls.length; i++) {
+        viewItemEls[i].addEventListener("click", onClickViewItem);
+    }
+    for (var j = 0; j < btnCloseEls.length; j++) {
+        btnCloseEls[j].addEventListener("click", onClickClose);
+    }
+}
+addEvent();
 
 
-        init();
-    });
-})(jQuery);
+
+
