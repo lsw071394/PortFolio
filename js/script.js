9771b4b28be921fe99103fc573e3a3aadbb793b3
lsw071394 <54699628+lsw071394@users.noreply.github.com>
2023-07-26 14:20:13 +0900

    Update script.js
    #23.07.26

--- js/script.js
+++ js/script.js
@@ -5,8 +5,6 @@ var rotateEl = document.querySelectorAll('#rotate');
 var reverseRotateEl = document.querySelectorAll('#reverse-rotate');
 var scrolliconEl = document.querySelector('.scrollicon');
 
-
-
 // 햄버거 메뉴
 var MenuEl = document.querySelector('.hamburger');
 var spanEL = document.querySelectorAll('span');
@@ -38,93 +36,97 @@ var htmlEl = document.documentElement,
     _timer;
 
 var contactEl = document.querySelector('.contact');
-var contactbodyEl =document.querySelector('.contactbody');
+var contactbodyEl = document.querySelector('.contactbody');
 var gear = document.querySelectorAll('.gear');
-
-
-
-
-reset();
-
-
-function reset(){ 
-    navEl.style.transform = 'translateX(100%)';
-    contactbodyEl.style.transform = 'translate(-50%,-50%) scale(0)';
-
-}
-
-window.onload = function() {
-    setTimeout (function () {
-        scrollTo(0,0);
+// reset();
+// function reset() {
+//     navEl.style.transform = 'translateX(100%)';
+//     contactbodyEl.style.transform = 'translate(-50%,-50%) scale(0)';
+// }
+window.onload = function () {
+    setTimeout(function () {
+        scrollTo(0, 0);
         articleEl[0].style.visibility = 'visible';
-    },50);
+    }, 50);
 }
-
-
 // 메인 톱니바퀴 모션 함수
-setTimeout(function(){
-    
-    gearEl.style.animation = 'rotateGear 4s linear infinite forwards';
-    for(var i = 0; i <rotateEl.length; i++){
-        rotateEl[i].style.animation = 'rotation 4s linear infinite forwards';
+setTimeout(function () {
+    gearEl.style.animation = 'rotateGear 2s linear infinite forwards';
+    for (var i = 0; i < rotateEl.length; i++) {
+        rotateEl[i].style.animation = 'rotation 2s linear infinite forwards';
     }
-    for(var i = 0; i <reverseRotateEl.length; i++){
-        reverseRotateEl[i].style.animation = 'reverse-rotation 4s linear infinite forwards';
-    }  
-    
-}, 3100);
-setTimeout(function(){
+    for (var i = 0; i < reverseRotateEl.length; i++) {
+        reverseRotateEl[i].style.animation = 'reverse-rotation 2s linear infinite forwards';
+    }
+}, 2100);
+
+setTimeout(function () {
     scrolliconEl.style.opacity = '1';
-}, 3500);
-
-
-
+}, 2500);
 
 // 햄버거 클릭시
-function MenuChange(e){
+function MenuChange(e) {
     e.preventDefault();
-    
     openmenu();
-    for(i=0; i < spanEL.length; i++){
+    for (i = 0; i < spanEL.length; i++) {
         spanEL[i].style.transition = 'all 0.5s ease-in-out';
     }
-    if(toggle){
+    if (toggle) {
         spanEL[0].style.transform = 'translateY(9px) rotateZ(45deg)';
-        spanEL[1].style.opacity= 0;
+        spanEL[1].style.opacity = 0;
         spanEL[2].style.transform = 'translateY(-9px) rotateZ(-45deg)';
         hamburger.style.background = '#444444';
         hamburger.style.boxShadow = '5px 5px 9px #2b2b2b inset, -5px -5px 9px #5d5d5d inset ';
-    }else if(!toggle){
+    } else if (!toggle) {
         spanEL[0].style.transform = '';
-        spanEL[1].style.opacity= 1;
+        spanEL[1].style.opacity = 1;
         spanEL[2].style.transform = '';
         hamburger.style.background = 'linear-gradient(145deg, #ffffff, #e6e6e6)';
-        hamburger.style.boxShadow ='5px 5px 9px #a1a1a1';
-
-        
+        hamburger.style.boxShadow = '5px 5px 9px #a1a1a1';
     }
     toggle = !toggle;
 }
-MenuEl.addEventListener('click',MenuChange);
+MenuEl.addEventListener('click', MenuChange);
+
+fatimesEl = document.querySelector('.fa-times');
+// fatimesEl.addEventListener('click', closecontact);
+// contactEl.addEventListener('click', opencontact);
+function closecontact() {
+    for (var i = 0; i < gear.length; i++) {
+        gear[i].style.opacity = '0.3';
+        gear[i].style.transition = 'all .5s ease-in'
+    }
+    contactbodyEl.style.transform = 'translate(-50%,-50%) scale(0)';
+    contactbodyEl.style.transition = 'all 0.3s ease-in-out';
+}
 
 
-
+function opencontact(e) {
+    for (var i = 0; i < gear.length; i++) {
+        gear[i].style.opacity = '1';
+        gear[i].style.transition = 'all 1s ease-in'
+    }
+    contactbodyEl.style.transform = 'translate(-50%,-50%) scale(1)';
+    contactbodyEl.style.opacity = '1';
+    // contactbodyEl.style.transform = 'translateY(0)';
+    contactbodyEl.style.transition = 'all 0.3s ease-in-out';
+}
 
 
 // 메뉴 오픈시
-function openmenu(){
-    if(toggle){
+function openmenu() {
+    if (toggle) {
         navEl.style.transform = 'scale(1)';
         navEl.style.opacity = '1';
         navEl.style.transform = 'translateX(0)';
         navMenuEl.style.display = 'block';
         navMenuEl.style.opacity = '0';
         navMenuEl.style.transition = 'opacity 0.3s ease-in-out';
-        setTimeout(function(){    
+        setTimeout(function () {
             navMenuEl.style.opacity = '1';
-        },300);
+        }, 500);
     }
-    else if(!toggle){
+    else if (!toggle) {
         navEl.style.transform = 'scale(0)';
         navEl.style.opacity = '0';
         navMenuEl.style.opacity = '0';
@@ -135,30 +137,23 @@ function openmenu(){
     navEl.style.transition = 'all 0.3s ease-in-out';
 }
 
-
-
-
-
-
-
-
 viewItemEls = Array.prototype.slice.call(viewItemEls);
 function onResize() {
     clearInterval(_timer);
     htmlEl.classList.add("on-resize");
-    _timer = setTimeout(function() {
+    _timer = setTimeout(function () {
         htmlEl.classList.remove("on-resize");
     }, 1);
 }
 function onClickViewItem(e) {
     e.preventDefault();
     var el = e.currentTarget, index = viewItemEls.indexOf(el);
-    if(!el.classList.contains("selected")) {
+    if (!el.classList.contains("selected")) {
         _cuId = index;
-        if(_exId !== null) viewItemEls[_exId].classList.remove("selected");
+        if (_exId !== null) viewItemEls[_exId].classList.remove("selected");
         el.classList.add("selected");
         _exId = _cuId;
-    }else if(el.classList.contains("selected")){
+    } else if (el.classList.contains("selected")) {
         el.classList.remove('selected');
     }
 }
@@ -166,18 +161,18 @@ function onClickClose(e) {
     e.preventDefault();
     e.stopPropagation();
     var el = e.currentTarget, itemEl = el.parentElement;
-    if(itemEl.classList.contains("selected")) {
-        if(_exId !== null) viewItemEls[_exId].classList.remove("selected");
+    if (itemEl.classList.contains("selected")) {
+        if (_exId !== null) viewItemEls[_exId].classList.remove("selected");
         _cuId = null;
         _exId = _cuId;
     }
 }
 function addEvent() {
     window.addEventListener("resize", onResize);
-    for(var i = 0; i < viewItemEls.length; i++) {
+    for (var i = 0; i < viewItemEls.length; i++) {
         viewItemEls[i].addEventListener("click", onClickViewItem);
     }
-    for(var j = 0; j < btnCloseEls.length; j++) {
+    for (var j = 0; j < btnCloseEls.length; j++) {
         btnCloseEls[j].addEventListener("click", onClickClose);
     }
 }
